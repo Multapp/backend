bf0574bdd2d8e678b58e40f9da77633365c964d5
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-18 15:57:47 -0300

    volviendo a devolver las multas resumidas

--- src/services/multasService.js
+++ src/services/multasService.js
@@ -18,15 +18,15 @@ module.exports = function (db) {
                     snapshot.forEach(multa => {
                         console.log("id", multa.id);
                         console.log("data", multa.data());
-                        /*let multaResumida = {
+                        let multaResumida = {
                             id: multa.id,
-                            nombreConductor: multa.data().conductor.nombre,
+                            nombreConductor: multa.data().conductor.apellido + " " + multa.data().conductor.nombre,
                             dniConductor: multa.data().conductor.nroDocumento,
                             fecha: multa.data().ubicacion.fecha,
                             extracto: multa.data().infraccion.extracto,
                             estado: multa.data().estado,
-                        };*/
-                        multasResumidas.push(multa.data());
+                        };
+                        multasResumidas.push(multaResumida);
                     });
                     res.send(multasResumidas);
                 }).catch(error => {
