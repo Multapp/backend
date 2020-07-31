1aa71f08e022b97eac50cde2579bc1e10dd25f32
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-30 22:43:29 -0300

    ahora addUsuario y deleteUsuario manejan la cuenta en authentication

--- src/services/usuariosService.js
+++ src/services/usuariosService.js
@@ -1,4 +1,4 @@
-module.exports = (db, admin) => {
+module.exports = (db, auth, storage) => {
     return {
         getUsuarioById: (req, res, next) => {
             db.collection("usuarios").doc(req.query.id).get()
@@ -33,23 +33,39 @@ module.exports = (db, admin) => {
             // aca tambien habria que:
             // guardar la foto del tipo en storage
             // mandarle correo al tipo con su contraseña
-            admin.auth().createUser({
+            auth.createUser({ // crea el usuario
                 email: req.body.usuario.email,
                 password: (Math.floor(Math.random() * (1000000 - 100000) ) + 100000).toString(),
                 displayName: req.body.usuario.nombre + " " + req.body.usuario.apellido,
                 phoneNumber: "+54" + req.body.usuario.telefono,
-                // photoURL: null,
             })
                 .then(userRecord => {
+                    console.log("uid", userRecord.uid);
+                    console.log("email", userRecord.email);
+                    console.log("password", userRecord.password);
                     let uid = userRecord.uid;
-                    admin.auth().setCustomUserClaims(uid, {rol: req.body.usuario.rol})
-                        .then(() => {
+                    auth.setCustomUserClaims(uid, {rol: req.body.usuario.rol}) // setea el rol del usuario
+                        .then(() => { // guarda los datos personales en la base de datos
                             db.collection("usuarios").add({
                                 ...req.body.usuario,
                                 uid: uid,
                             })
                                 .then(() => {
-                                    res.status(201).send("Usuario " + uid + " creado correctamente");
+                                    // storage.ref().child("avatar/" + uid).put(req.body.foto) // guarda la foto de perfil
+                                    //     .then(snapshot => {
+                                    //         console.log(snapshot);
+                                            // userRecord.updateUser({ // guarda la referencia a la foto en authentication
+                                            //     // photoURL
+                                            // })
+                                            //     .then(() => {
+                                                    res.status(201).send("Usuario " + uid + " creado correctamente");
+                                                // }).catch(error => {
+                                                //     res.status(500).send(error);
+                                                // });
+                                        // }).catch(error => {
+                                        //     console.log(error);
+                                        //     res.status(500).send(error);
+                                        // })
                                 }).catch(error => {
                                     console.log(error);
                                     res.status(500).send(error);
@@ -76,13 +92,29 @@ module.exports = (db, admin) => {
                 });
         },
         deleteUsuario: (req, res, next) => {
-            db.collection("usuarios").doc(req.query.id).delete()
+            // FALTA
+            // eliminar la foto de perfil del usuario (cuando sepa como cargar una)
+            let uid;
+            db.collection("usuarios").doc(req.query.id).get() // trae el uid guardado en firestore
                 .then(snapshot => {
-                    res.send("Usuario " + req.query.id + " eliminado correctamente");
+                    uid = snapshot.data().uid;
+                    auth.deleteUser(uid) // elimina la cuenta de authentication
+                        .then(() => {
+                            db.collection("usuarios").doc(req.query.id).delete() // elimina todos los datos del usuario en firestore
+                                .then(() => {
+                                    res.status(200).send("Usuario " + req.query.id + " eliminado correctamente");
+                                }).catch(error => {
+                                    console.log(error);
+                                    res.status(500).send("Error al eliminar datos de usuario " + res.query.id, error);
+                                });
+                        }).catch(error => {
+                            console.log(error);
+                            res.status(500).send("Error al eliminar cuenta " + uid, error);
+                        });
                 }).catch(error => {
-                    res.send("Error al eliminar usuario " + res.query.id, error);
+                    console.log(error);
+                    res.status(500).send("Error al recuperar el UID del registro " + req.query.id, error);
                 });
-            
         }
     }
 }
