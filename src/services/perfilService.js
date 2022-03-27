14b1f6eff2b5449db6eb4e0c3d2eb01396e2b44b
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2022-03-27 20:39:25 -0300

    refactorizando

--- src/services/perfilService.js
+++ src/services/perfilService.js
@@ -2,26 +2,27 @@ module.exports = (db, auth) => {
     return {
         getPerfil: async (req, res) => {
             try {
-                const userRecord = await auth.getUser(req.query.uid);
+                const [userRecord, snapshot] = await Promise.all([
+                    auth.getUser(req.query.uid),
+                    db.collection("usuarios").doc(userRecord.uid).get()
+                ]);
                 const response = {
                     id: userRecord.uid,
                     datos: null,
                     foto: userRecord.photoURL,
+                    datos: {
+                        nombre: userRecord.displayName,
+                        rol: userRecord.customClaims.rol,
+                        email: userRecord.email,
+                        dni: snapshot.data().dni,
+                        fechaNacimiento: snapshot.data().fechaNacimiento,
+                        sexo: snapshot.data().sexo,
+                        telefono: userRecord.phoneNumber,
+                        direccion: snapshot.data().calle + " " + snapshot.data().numero,
+                        localidad: snapshot.data().localidad,
+                        provincia: snapshot.data().provincia
+                    }
                 };
-                const snapshot = await db.collection("usuarios").doc(userRecord.uid).get();
-                const datos = {
-                    nombre: userRecord.displayName,
-                    rol: userRecord.customClaims.rol,
-                    email: userRecord.email,
-                    dni: snapshot.data().dni,
-                    fechaNacimiento: snapshot.data().fechaNacimiento,
-                    sexo: snapshot.data().sexo,
-                    telefono: userRecord.phoneNumber,
-                    direccion: snapshot.data().calle + " " + snapshot.data().numero,
-                    localidad: snapshot.data().localidad,
-                    provincia: snapshot.data().provincia,
-                }
-                response.datos = datos;
                 res.send(response);
             } catch (err) {
                 console.log(err);
